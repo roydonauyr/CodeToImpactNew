ad05e23e7de0dea9a523c4470739ba1264ebcf72
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-16 00:22:49 +0800

    Refactor folder structure of client

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 
-import Register from './Auth/Register';
-import Login from './Auth/Login';
+import Register from './pages/register-page'
+import Login from './pages/login-page'
 
-import "./App.css";
+import './App.css'
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Login />} />
-				<Route path='/register' element={<Register />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
