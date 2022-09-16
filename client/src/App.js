1ddb18363c8c16703e4bc2597252a57d7695df3f
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-16 19:28:09 +0800

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Register from './pages/register-page'
 import Login from './pages/login-page'
-import DashBoard from './pages/dashboard-page'
+// import DashBoard from './pages/dashboard-page'
 
 import './App.css'
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<DashBoard />} />
+        {/* <Route path="/dashboard" element={<DashBoard />} /> */}
       </Routes>
     </BrowserRouter>
   )
