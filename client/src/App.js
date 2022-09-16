ae3ab1d63e182cbb6c1d0f05fb590794b20c4ecc
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-16 21:55:27 +0800

    Fix client folder pathing issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Register from './pages/register-page/index'
 import Login from './pages/login-page/index'
-// import DashBoard from './pages/dashboard-page/index'
+import DashBoard from './pages/dashboard-page/index'
 
 import './App.css'
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route path="/dashboard" element={<DashBoard />} /> */}
+        <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </BrowserRouter>
   )
